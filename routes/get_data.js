6146eb5950645c298d31c4b83a4ff8ffbce862bd
irisrumtub <tvburis@gmail.com>
2023-07-21 12:43:05 +0300

    sync

--- routes/get_data.js
+++ routes/get_data.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const msgsAndMemesByDay = require('../helpers/dateMesMem/dateMesMem')
+const dailyUserStats = require('../helpers/dailyUserStats/2')
 
-//
+/* "21/01/2023": { memes: 1, msgs: 2 },
+    "21/01/2023": { memes: 1, msgs: 2 }
+}*/
 router.get('/dateMesMem', async (req, res, next) => { 
    
     const startDate = req.query.start
@@ -19,4 +22,19 @@ router.get('/dateMesMem', async (req, res, next) => {
     }
 });
 
+
+router.get('/dailyUserStats', async (req, res, next) => { 
+   
+    const startDate = req.query.start
+    const endDate = req.query.end
+
+    try {
+        const response = await dailyUserStats(startDate, endDate); 
+        res.json(response); 
+    } catch (err) {
+        
+        console.error('Error:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 module.exports = router;
